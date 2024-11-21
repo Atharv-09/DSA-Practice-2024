57f12f1c169f867c803ea311586424e9f6933c30
Atharva Shinde <atharva09shinde@gmail.com>
2024-11-21 13:37:24 +0530

    added code for brute force approach

--- LeaderInArray.java
+++ LeaderInArray.java
@@ -2,12 +2,42 @@
 
 import java.util.*;
 public class LeaderInArray {
+    // Brute force approach
+    // TC : O(N^2)
+// using 2 for loop check each element is greater then its rightmost element if yes then add it to ans
+        // TIME : O(N^2) Space : O(N) -> for ans
+    public static ArrayList<Integer> 
+                     printLeadersBruteForce(int[] arr, int n){
     
+        ArrayList<Integer> ans= new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            boolean leader = true;
+
+            //Checking whether arr[i] is greater than all 
+            //the elements in its right side
+            for (int j = i + 1; j < n; j++)
+            if (arr[j] > arr[i]) {
+                
+                // If any element found is greater than current leader
+                // curr element is not the leader.
+                leader = false;
+                break;
+            }
+
+            // Push all the leaders in ans array.
+            if (leader)
+            ans.add(arr[i]);
+
+        }
+        
+        return ans;
+        
+        }
     
     static void findLeaderInArray(int[] arr,int n){
 
-        // using 2 for loop check each element is greater then its rightmost element if yes then add it to ans
-        // TIME : O(N^2) Space : O(N) -> for ans
+        
 
         int maxEleToRight = Integer.MIN_VALUE;
         ArrayList<Integer> ans = new ArrayList<>();
