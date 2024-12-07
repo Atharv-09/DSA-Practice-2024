496e377331003393f27cb99ade24b1110d321f2c
Atharva Shinde <atharva09shinde@gmail.com>
2024-12-07 16:43:42 +0530

    Code added.

---
+++ Stack/NumberOfNGEtoRight.java
@@ -0,0 +1,16 @@
+/*Given an array of N integers and Q queries of indices. Return a list NGEs[] where NGEs[i] stores the count of elements strictly greater than the current element (arr[indices[i]]) to the right of indices[i].
+Input:  arr[]     = [3, 4, 2, 7, 5, 8, 10, 6]
+        queries = 2
+        indices[] = [0, 5]
+Output:  6, 1
+Explanation: The next greater elements to the right of 3(index 0) are 4,7,5,8,10,6. The next greater elements to the right of 8(index 5) is only 10.
+
+*/
+
+//PROBLEM : 
+// we will have the arr and indices such that
+// for that index we have to check how many next greater element are thier to its right
+
+public class NumberOfNGEtoRight {
+    
+}
